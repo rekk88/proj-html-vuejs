5abddcb3e2dd04f946172de4d3b12c6a1d2abb81
maicol <maicol.matteoli@gmail.com>
2021-07-16 17:34:04 +0200

    add : fontawesome , colors , style header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,11 +2,13 @@
   <div class="header_wrap ">
       <!-- Header -->
       <div class="container d-flex justify-content-around align-items-center">
-            <div class="img">
-                <img :src="require('@/assets/images/logo.png')" alt="logo">
+            <div class="img_container">
+                <img class="p-2" :src="require('@/assets/images/logo.png')" alt="logo">
             </div>
-            <ul class="m-0">
-                <li v-for="(element,index) in menu" :key="index">{{element}}</li>
+            <ul class="m-0 px-0">
+                <li v-for="(element,index) in menu" :key="index" class="px-3">{{element}}</li>
+                <li class="ps-1"><span class="btn btn-dark px-4 text-uppercase">join us</span></li>
+                <li><i class="fas fa-search ps-5 fs_6 align-middle"></i></li>
             </ul>
       </div>
       
@@ -27,6 +29,29 @@ export default {
 @import '@/style/colors.scss';
     .header_wrap{
         background-color: $eastern_blue;
+        .container{
+            height: 70px;
+            .img_container{
+                height: 100%;
+                img{
+                    height: 100%;
+                }
+            }
+            ul{
+                font-size: 0.95rem;
+                color: $white;
+                font-weight: bold;
+
+                .btn.btn-dark{
+                    font-weight: bold !important;
+                    &:hover{
+                        background-color: $salmon;
+                        border-color: $salmon;
+                    }
+                }
+            }
+
+        }
     }
     li{
         list-style: none;
