fc0d6bd336892472c0aea70376d37407d507e0ff
rekk88 <maicol.matteoli@gmail.com>
2021-07-20 15:08:47 +0200

    add : News.vue + SectionTitle component

---
+++ src/components/News.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+      <SectionTitle :titoloh6="'PHASELLUS EGET METUS'" :titoloH2="'All the latest news'"/>
+      
+  </div>
+</template>
+
+<script>
+import SectionTitle from './SectionTitle.vue'
+
+
+export default {
+name:"News",
+props:{
+
+},
+components:{
+    SectionTitle
+}
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
